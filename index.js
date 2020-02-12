61235affadeaddabfaaff12184be8f80fd609ef6
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-12 18:45:47 -0500

    everything but get user works

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code away!
+const express = require('express')
+const server = express()
+
+const userRouter = require('./users/userRouter')
+
+
+function logger(req,res,next){
+    console.log(`${req.method} to ${req.url}`)
+}
+
+server.use(express.json())
+
+server.use('/api/users', userRouter)
+
+server.get('/', (req, res) => {
+    res.json({ message: 'Success'})
+})
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`server is listening on port ${port}`)
+    
+})
+
+server.use(logger)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,152 @@
 const express = require('express');
-
+   
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const userDB = require('./userDb')
+const postDB = require('../posts/postDb')
+
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  const newUser = req.body;
+
+  userDB.insert(newUser)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({errorMessage: "cannot create user"})
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const newPost = req.body;
+
+  postDB.insert(newPost)
+  .then(post => {
+    res.status(201).json(post)
+  })
+  .catch( err => {
+    res.status(500).json({errorMessage: "cannot create post"})
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  userDB.get()
+  .then( user => {
+    res.status(200).json(users)
+  })
+  .catch( err => {
+    res.status(500).json({errorMessage: "cannot access user"})
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id;
+
+  userDB.getById(id)
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({errorMessage: "cannot access user"})
+  })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id;
+
+  userDB.getUserPosts(id)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => {
+    res.status(500).json({ errorMessage: "cannot access user's post"})
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const user = req.user;
+  userDB.remove(user.id)
+  .then(deleted => {
+    if(deleted){
+      res.status(204).end()
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ errorMessage: "cannot delete user"})
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const user = req.user;
+  const updated = req.body;
+
+  userDB.update(user.id, updated)
+  .then(changed => {
+    if(changed){
+      userDB.getById(user.id)
+      .then( updatedUser => {
+        res.status(200).json(updatedUser)
+      })
+      .catch(err => {
+        res.status(404).json({errorMessage: "cannot edit user"})
+      })
+    } else {
+      res.status(500).json({ errorMessage: "user cannot be changed"})
+    }
+  })
+  .catch( err => {
+    res.status
+  })
+
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const id = req.params.id || req.body.user_id;
+
+  userDB.getById(id)
+  .then(user => {
+    if(user){
+      return next();
+    } else {
+      res.status(404).json({ message: "invalid user"})
+    }
+  })
+  .catch(() => {
+    res.status(500).json({message: "User is not found"})
+  })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if(!req.body){
+    res.status(400).json({ message: "user is not found"})
+  }
+  if(!req.body.name) {
+    res.status(400).json({ message: "must provide name"})
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body){
+    res.status(400).json({ message: "cannot find post"})
+  }
+  if(!req.body.text){
+    res.status(400).json({ message: "required text field"})
+  }
+  next();
 }
 
 module.exports = router;
